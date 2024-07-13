2ac89c3f5505c9e4b42440d7adef539e83395c1e
mohdbinharun56 <arahaman461@gmail.com>
2024-07-13 13:16:15 +0600

    update app.module.ts connection with postgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminManager, Agent, Bus, Customer, CustomerAgent, Feedback, Info, Login, Manager, ManagerAgent, Notification, Report, Ticket, TicketBookInfo } from './entity';
+import { Admin } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'admin',
+    database: 'ticket',
+    entities: [
+      Login,
+      Info,
+      Admin,
+      Manager,
+      Customer,
+      Agent,
+      CustomerAgent,
+      AdminManager,
+      ManagerAgent,
+      Bus,
+      Ticket,
+      Feedback,
+      Report,
+      Notification,
+      TicketBookInfo,
+    ],
+    autoLoadEntities: true,
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
